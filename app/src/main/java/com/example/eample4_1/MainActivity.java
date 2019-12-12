b741bc77a32a733dd5feecb01a275eae91786da0
sungdong9kim <kikikikihi@naver.com>
2019-12-13 03:15:57 +0900

    Example for test

---
+++ app/src/main/java/com/example/eample4_1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.eample4_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static void main(String[] args) {
+        double S=6;
+        double P=0.75;
+        double L=200;
+        double T=0;
+        double A=1;
+        double Y=1;
+        double ups1=P*L*(Y+9);
+        double downs=(T+1)*(A+1)*(Y+10)*(L+20);
+        double ups2=12+3*S/8.0;
+        double high = ups1 / downs *ups2;
+        System.out.printf("적당한 하이힐 높이는 %1$.2fcm입니다.", high);
+
+    }
+
+}
